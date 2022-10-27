2fae0ebd42305d0985e3806d1ac7884269000dbf
Baran Kahraman <97622903+Kladenste1n@users.noreply.github.com>
2022-10-28 00:27:22 +0300

    Update Seven Segment Display.asm

--- Seven Segment Display.asm
+++ Seven Segment Display.asm
@@ -1,3 +1,10 @@
+/*
+Write a main program to display all the courses you are currently taking this semester one by one in four digits using four seven-segment displays. 
+For example, if the currently taken courses are EEE212, EEE202, MATH242, HUM112, ENG401, then we would be continuously displaying the
+pattern 0212, 0202, 1242, 2112, 3401, 0212, 0202, on the four seven segment displays. For other departments, use other codes, i..e., most significant digit, you find
+appropriate. 
+*/
+
 ORG 0
 THERE:
 DIGIT3:MOV R3,#00H
